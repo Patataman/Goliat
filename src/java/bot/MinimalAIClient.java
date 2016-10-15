808cb5a3a48b236e9dcb2f14ef82e7e2780842a6
Patataman IV <Patataman IV@DESKTOP-3B9VVD8>
2016-10-15 17:06:07 +0200

    Init

---
+++ src/java/bot/MinimalAIClient.java
@@ -0,0 +1,73 @@
+package bot;
+
+/**
+ * Example of a Java AI Client that does nothing.
+ */
+import jnibwapi.BWAPIEventListener;
+import jnibwapi.JNIBWAPI;
+import jnibwapi.Position;
+import jnibwapi.Unit;
+import jnibwapi.util.BWColor;
+
+public class MinimalAIClient implements BWAPIEventListener {
+	private JNIBWAPI bwapi;
+	
+	public static void main(String[] args) {
+		new MinimalAIClient();
+	}
+	
+	public MinimalAIClient() {
+		bwapi = new JNIBWAPI(this, false);
+		bwapi.start();
+	}
+	
+	@Override
+	public void connected() {}
+	
+	@Override
+	public void matchStart() {}
+	
+	@Override
+	public void matchFrame() {
+		for (Unit u : bwapi.getAllUnits()) {
+			bwapi.drawCircle(u.getPosition(), 5, BWColor.Red, true, false);
+		}
+	}
+	
+	@Override
+	public void keyPressed(int keyCode) {}
+	@Override
+	public void matchEnd(boolean winner) {}
+	@Override
+	public void sendText(String text) {}
+	@Override
+	public void receiveText(String text) {}
+	@Override
+	public void nukeDetect(Position p) {}
+	@Override
+	public void nukeDetect() {}
+	@Override
+	public void playerLeft(int playerID) {}
+	@Override
+	public void unitCreate(int unitID) {}
+	@Override
+	public void unitDestroy(int unitID) {}
+	@Override
+	public void unitDiscover(int unitID) {}
+	@Override
+	public void unitEvade(int unitID) {}
+	@Override
+	public void unitHide(int unitID) {}
+	@Override
+	public void unitMorph(int unitID) {}
+	@Override
+	public void unitShow(int unitID) {}
+	@Override
+	public void unitRenegade(int unitID) {}
+	@Override
+	public void saveGame(String gameName) {}
+	@Override
+	public void unitComplete(int unitID) {}
+	@Override
+	public void playerDropped(int playerID) {}
+}
