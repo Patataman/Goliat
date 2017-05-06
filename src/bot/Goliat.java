f36472785ae1e4ff190731020a45f7a58353270f
Patataman <shas_arroyo@hotmail.com>
2017-05-06 17:33:17 +0200

    waste import

--- src/bot/Goliat.java
+++ src/bot/Goliat.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.function.Predicate;
 
 import org.iaie.Agent;
